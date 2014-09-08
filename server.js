019e04c1047cb89974be34c0a98e7cb141efb0f2
Brett McCaffray <Brett@bmp.local>
2014-09-07 20:26:56 -0700

    edit text to json object & date to page load not server load

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var http = require('http');
 
 var app = express();
 
-var date = new Date();
-var time = date.getHours() + ":" + date.getMinutes();
-
 app.get('/', function(req, res) {
-	res.send(time);
+	var date = new Date();
+	var time = date.getHours() + ":" + date.getMinutes();
+	res.send({'time': time});
 });
 
 app.get('/:nameInput', function(req, res) {
@@ -21,5 +20,5 @@ var server = http.createServer(app);
 app.set('port', process.env.PORT || 3000);
 
 server.listen(app.get('port'), function() {
-  console.log('server running on port ' + app.get('port'));
+	console.log('server running on port ' + app.get('port'));
 });
